1486754d75dd65fdd6ef700b31e02e067456c5ee
Giovanni Calò <giovanni.calo@live.it>
2025-05-04 15:36:12 +0100

    Added types

---
+++ source/index.d.ts
@@ -0,0 +1,34 @@
+type Region = {
+	height: number;
+	id: number;
+	source: number;
+	time: number;
+	width: number;
+	x: number;
+	y: number;
+};
+
+type Regions = {
+	[key: number]: Region;
+};
+
+type ValidState = {
+	height: number;
+	isValid: true;
+	regions: Regions;
+	time: number;
+	width: number;
+};
+
+type InvalidState = {
+	isValid: false;
+};
+
+type State = ValidState | InvalidState;
+
+declare class Parser {
+	constructor();
+	parse(data: Uint8Array): Promise<State>;
+}
+
+export = Parser;
